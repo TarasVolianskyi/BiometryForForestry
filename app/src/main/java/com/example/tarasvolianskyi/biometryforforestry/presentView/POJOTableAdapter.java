732a9bdd41f421d3b730ece59c391e3ecac84b66
tv <volianskyitv@gmail.com>
2019-01-18 23:48:03 +0200

    refactoring of the structure

---
+++ app/src/main/java/com/example/tarasvolianskyi/biometryforforestry/presentView/POJOTableAdapter.java
@@ -0,0 +1,74 @@
+package com.example.tarasvolianskyi.biometryforforestry.presentView;
+
+public class POJOTableAdapter {
+
+    private String textOne;
+    private double num2;
+    private String textThree;
+    private double num4;
+    private double num5;
+
+
+    public POJOTableAdapter(String textOne, double num2, String textThree, double num4, double num5) {
+        this.textOne = textOne;
+        this.num2 = num2;
+        this.textThree = textThree;
+        this.num4 = num4;
+        this.num5 = num5;
+    }
+
+    public POJOTableAdapter() {
+
+    }
+
+    public void setTextOne(String textOne) {
+        this.textOne = textOne;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    public void setTextThree(String textThree) {
+        this.textThree = textThree;
+    }
+
+    public void setNum4(double num4) {
+        this.num4 = num4;
+    }
+
+    public void setNum5(double num5) {
+        this.num5 = num5;
+    }
+
+    public String getTextOne() {
+        return textOne;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public String getTextThree() {
+        return textThree;
+    }
+
+    public double getNum4() {
+        return num4;
+    }
+
+    public double getNum5() {
+        return num5;
+    }
+
+    @Override
+    public String toString() {
+        return "POJOTableAdapter{" +
+                "textOne='" + textOne + '\'' +
+                ", num2=" + num2 +
+                ", textThree='" + textThree + '\'' +
+                ", num4=" + num4 +
+                ", num5=" + num5 +
+                '}';
+    }
+}
--- app/src/main/java/com/example/tarasvolianskyi/biometryforforestry/presentView/PVTopic21.java
+++ app/src/main/java/com/example/tarasvolianskyi/biometryforforestry/presentView/PVTopic21.java
@@ -1,12 +1,17 @@
 package com.example.tarasvolianskyi.biometryforforestry.presentView;
 
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.example.tarasvolianskyi.biometryforforestry.IncomingData.MyArrayData;
@@ -36,13 +41,19 @@ public class PVTopic21 extends Fragment {
     private TextView tvNgranY;
     private TextView tvVgranX;
     private TextView tvVgranY;
+    private TableLayout tableLayout21;
 
+
+    private ArrayList<POJOTableAdapter> arrayList;
+    private TableLayout tableLayout;
+    private POJOTableAdapter pojoTableAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.topic_2_1_view_frament, container, false);
         getArray();
+        fillTableLayout();
         initView();
         return view;
     }
@@ -61,7 +72,7 @@ public class PVTopic21 extends Fragment {
         tvNgranY = view.findViewById(R.id.tv14_topic_2_1_view_fragment);
         tvVgranX = view.findViewById(R.id.tv16_topic_2_1_view_fragment);
         tvVgranY = view.findViewById(R.id.tv17_topic_2_1_view_fragment);
-
+        tableLayout21 = view.findViewById(R.id.tl_topic21_view_fragment);
 
         tvLgN.setText((Constants.K_FIRST_KOEF + String.valueOf(new BLTopic21().countLgN()) + " " +
                 Constants.NEAR_EQUAL +
@@ -99,4 +110,61 @@ public class PVTopic21 extends Fragment {
     }
 
 
+    @SuppressLint("SetTextI18n")
+    private void fillTableLayout() {
+       // fillData();
+        for (int i = 0; i < 6; i++) {
+            TableRow localRow = new TableRow(getActivity());
+            localRow.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 100));
+            //fillLocalRow(localRow, arrayList.get(i));
+            //TextView textView = new TextView(getActivity());
+
+            TextView tv1 = new TextView(getActivity());
+         /*   TextView tv2 = new TextView(getActivity());
+            TextView tv3 = new TextView(getActivity());
+            TextView tv4 = new TextView(getActivity());
+            TextView tv5 = new TextView(getActivity());
+*/
+            tv1.setText("1rrr");
+         /*   tv2.setText("r2rr");
+            tv3.setText("rr3r");
+            tv4.setText("rrr4");
+            tv5.setText("rrr5");
+*/
+
+            // textView.setLayoutParams(new ViewGroup.LayoutParams(200, 100));
+            //textView.setText(arrayList.get(i).toString());
+            localRow.addView(tv1);
+       /*     localRow.addView(tv2);
+            localRow.addView(tv3);
+            localRow.addView(tv4);
+            localRow.addView(tv5);
+            */
+            //Log.d("log apartm", arrayList.get(i).toString());
+            //Log.d("log apartm", "Table chield " + tableLayout.getChildCount());
+            localRow.setBackgroundColor(Color.GREEN);
+            tableLayout.addView(localRow, i);
+        }
+    }
+
+
+    private void fillData() {
+      /*  arrayList = new ArrayList<>();
+        arrayList.add(new POJOTableAdapter("ttt", 23, "ttt", 230000, 333));
+        arrayList.add(new POJOTableAdapter("rr", 33, "www", 250000, 44));
+        arrayList.add(new POJOTableAdapter("yyh", 43, "xxx", 210000, 555));
+        arrayList.add(new POJOTableAdapter("vvv", 53, "ggg", 560000, 232));
+        arrayList.add(new POJOTableAdapter("ywyh", 443, "xxfdx", 213000, 505));
+*/
+
+/*            pojoTableAdapter.setTextOne("eee");
+            pojoTableAdapter.setTextOne("eee");
+            pojoTableAdapter.setTextOne("eee");
+            pojoTableAdapter.setTextThree("eeqqs");
+            pojoTableAdapter.setTextThree("eeqqs");
+
+*/
+
+
+    }
 }
