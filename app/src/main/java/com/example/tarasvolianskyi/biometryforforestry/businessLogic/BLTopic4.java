1a6ab228482e564cc45772eaf5fdbd9042bd04aa
tv <volianskyitv@gmail.com>
2019-05-08 21:24:49 +0300

    refactoring of the structure

--- app/src/main/java/com/example/tarasvolianskyi/biometryforforestry/businessLogic/BLTopic4.java
+++ app/src/main/java/com/example/tarasvolianskyi/biometryforforestry/businessLogic/BLTopic4.java
@@ -1,6 +1,10 @@
 package com.example.tarasvolianskyi.biometryforforestry.businessLogic;
 
+import com.example.tarasvolianskyi.biometryforforestry.IncomingData.MyArrayData;
+import com.example.tarasvolianskyi.biometryforforestry.presentView.POJOTableAdapters.POJOTableAdapter;
 import com.example.tarasvolianskyi.biometryforforestry.presentView.POJOTableAdapters.POJOTableAdapter4;
+
+import java.util.ArrayList;
 
 public class BLTopic4 {
 
@@ -20,5 +24,18 @@ public class BLTopic4 {
 
     public double getXmaxTopic4() {
         return Math.round((XSerArefm + 3 * sigmaTopic4) * 10.0) / 10.0;
+    }
+
+
+    public ArrayList<POJOTableAdapter> showListInTable() {
+        ArrayList<POJOTableAdapter> arrayListPOJO = new ArrayList<>();
+      /*  ArrayList<Integer> arrayListOfCountedRozrads = (ArrayList<Integer>) sortDuplicatsOfRozrads(findNumbOfRozradForAllItems(new MyArrayData().getDataArrayDiametr()));
+        ArrayList<Integer> arrayListLastColomn = (ArrayList<Integer>) lastColomnTopic21(arrayListOfCountedRozrads);
+        for (int i = 0; i < arrayListOfCountedRozrads.size(); i++) {
+            arrayListPOJO.add(new POJOTableAdapter((countX1() + countCx() * i - countCx() / 2) + " - " + ((countX1() + countCx() * i + countCx() / 2) - 0.1),
+                    (countX1() + countCx() * i), "    ", arrayListOfCountedRozrads.get(i), arrayListLastColomn.get(i)));}
+*/
+
+        return arrayListPOJO;
     }
 }
